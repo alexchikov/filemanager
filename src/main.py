2a7bd1a275bccde2a04c0f3e1a7b28d836919e37
alexchikov <alex.chikov@outlook.com>
2023-09-06 23:31:01 +0300

    Continue working on functions

--- src/main.py
+++ src/main.py
@@ -4,13 +4,14 @@ import exc
 import traceback
 
 system = os.name
-banned_filenames = ['CON', 'PRN', 'AUX', 'NUL', 
-                    'COM0', 'COM1', 'COM2', 'COM3', 
-                    'COM4', 'COM5', 'COM6', 'COM7', 
+banned_filenames = ['CON', 'PRN', 'AUX', 'NUL',
+                    'COM0', 'COM1', 'COM2', 'COM3',
+                    'COM4', 'COM5', 'COM6', 'COM7',
                     'COM8', 'COM9', 'LPT0', 'LPT1',
                     'LPT2', 'LPT3', 'LPT4', 'LPT5',
                     'LPT6', 'LPT7', 'LPT8', 'LPT9']
-filename_regexp = re.compile(r'[^<>:«|?*.«:;|=,]+')
+dirname_regexp = re.compile(r'[^<>:«|?*.«:;|=,]+')
+filename_regexp = re.compile(r'[-a-zA-Z0-9()!@$%^*_.]+')
 
 """
 Commands:
@@ -26,66 +27,94 @@ Commands:
     > rname - for renaming files;
 """
 
+
 def parse_command(command: str) -> None:
     splitted = command.split()
     if splitted[0] in commands.keys():
         match splitted[0]:
             case 'mkd':
-                if len(splitted) == 2 and filename_regexp.fullmatch(splitted[1].strip('"')):
+                if len(splitted) == 2 and dirname_regexp.fullmatch(splitted[1].strip('"'))\
+                        and splitted[1] not in banned_filenames:
                     commands['mkd'](splitted[1])
                 else:
                     raise exc.CommandWrongArguments("Wrong arguments was handled")
             case 'dld':
-                if len(splitted) == 2 and filename_regexp.fullmatch(splitted[1].strip('"')):
+                if len(splitted) == 2 and dirname_regexp.fullmatch(splitted[1].strip('"'))\
+                        and splitted[1] not in banned_filenames:
                     commands['dld'](splitted[1])
                 else:
                     raise exc.CommandWrongArguments("Wrong arguments was handled")
-                    
+            case 'chd':
+                if len(splitted) == 2:
+                    commands['chd'](splitted[1])
+                else:
+                    raise exc.CommandWrongArguments("Wrong arguments was handled")
+            case 'mkf':
+                if len(splitted) == 2 and filename_regexp.fullmatch(splitted[1].strip('"')):
+                    commands['mkf'](splitted[1])
+                else:
+                    raise exc.CommandWrongArguments("Wrong arguments was handled: wrong filename")
+
     else:
         raise exc.CommandDoesNotExist("This command doesn't exist")
 
+
 def mkd(path: str) -> None:
     os.mkdir(path)
-    
+
+
 def dld(path: str):
     try:
         os.rmdir(path=path)
     except OSError:
         raise exc.CommandWrongArguments("This file doesn't exist")
-    
-def chd():
-    ...
-    
-def mkf():
-    ...
+
+
+def chd(path):
+    try:
+        os.chdir(path=path)
+    except OSError:
+        raise exc.CommandWrongArguments("This dir doesn't exist")
+
+
+def mkf(path):
+    open(path, 'w')
+
 
 def wrt():
     ...
 
+
 def dlf():
     ...
 
+
 def view():
     ...
-    
+
+
 def copy():
     ...
-    
+
+
 def move():
     ...
-    
+
+
 def rname():
     ...
-    
+
+
 def run() -> str:
-    command = input(f'\033[32m(filemanager) \033[0m')
+    command = input(f'\033[32m(filemanager\033[36m|\033[32m{os.getcwd()})\033[0m ')
     try:
         parse_command(command=command)
     except:
-        print(f"\033[33m{traceback.format_exc()}")
+        print(f"\033[31m{traceback.format_exc()}", end='')
+
 
 commands = {'mkd': mkd, 'dld': dld,
-            'chd':chd, 'mkf': mkf,
+            'chd': chd, 'mkf': mkf,
             'wrt': wrt, 'dlf': dlf,
             'view': view, 'copy': copy,
             'move': move, 'rname': rname}
